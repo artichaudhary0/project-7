25e295086993f12068d3bc31f2fee2d07db41eeb
Arti chaudhary <chaudhary.aarti1998@gmail.com>
2025-02-09 22:45:36 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Provider } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { store } from './app/store';
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginSuccess, loginFailure, signupSuccess, signupFailure, logout } from '../features/authSlice';
 
